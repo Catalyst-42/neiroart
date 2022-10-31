80dd907aa9033409d43a70b00ad3b1fbade4bb81
grigorijtomczuk <grigorijtomczuk@gmail.com>
2022-10-31 15:50:33 +0300

    Updated scripts, README.md
    Modified the scripts to be cleaner, modified README.md and separated README_ru.md, added logo.png, added requirements.txt and .gitignore

--- map_linear.py
+++ map_linear.py
@@ -1,7 +1,7 @@
 from random import choice, randint
-from PIL import Image
-from math import *
+
 import numpy
+from PIL import Image
 
 MAX_X = 256
 MAX_Y = 256
@@ -24,21 +24,19 @@ color_names = (
 
 data = numpy.zeros((MAX_X, MAX_Y, 3), dtype=numpy.uint8)
 data[:][:] = colors_RGB['bg']
-image = Image.fromarray(data)
+color = colors_RGB['blue']
 
 def randcolor(): return colors_RGB[choice(color_names)]
 
-color = colors_RGB['blue']
 def draw_side(x, y, side):
     global color
-
     if randint(1, 100) == 1:
-        color = colors_RGB[choice(color_names)]
+        color = randcolor()
 
-    def right_enter(): data[x:x+3, y] = color #colors_RGB['blue'] # right
-    def left_enter(): data[x-2:x+1, y] = color #colors_RGB['blue'] # left
-    def up_enter(): data[x, y:y+3] = color #colors_RGB['blue'] # up
-    def down_enter(): data[x, y-2:y+1] = color #colors_RGB['blue'] # down
+    def right_enter(): data[x:x+3, y] = color # right
+    def left_enter(): data[x-2:x+1, y] = color # left
+    def up_enter(): data[x, y:y+3] = color # up
+    def down_enter(): data[x, y-2:y+1] = color # down
 
     if side in (1, 2, 4, 8, 11, 12, 13, 15): 
         right_enter()
@@ -67,55 +65,52 @@ def draw_side(x, y, side):
 x, y = MAX_X//2, MAX_Y//2
 working_stack = []
 iters = 0
+
 draw_side(x, y, choice((1, 12, 13, 14, 15)))
+
 while len(working_stack):
     iters += 1
     x, y = working_stack.pop(randint(0, len(working_stack)-1))
-    # x, y = working_stack.pop()
     choice_side = [_ for _ in range(1, 16)]
 
     # right
     if data[x+6, y][0] != 21 and data[x+4, y][0] == 21:
-        for _ in (1, 2, 4, 8, 11, 12, 13, 15): 
+        for _ in (1, 2, 4, 8, 11, 12, 13, 15):
             if _ in choice_side: choice_side.remove(_)
-
     elif data[x+6, y][0] != 21 and data[x+4, y][0] != 21:
-        for _ in (0, 3, 5, 6, 7, 9, 10, 14): 
+        for _ in (0, 3, 5, 6, 7, 9, 10, 14):
             if _ in choice_side: choice_side.remove(_)
 
     # left
     if data[x-6, y][0] != 21 and data[x-4, y][0] == 21:
-        for _ in (1, 2, 6, 9, 10, 13, 14, 15): 
+        for _ in (1, 2, 6, 9, 10, 13, 14, 15):
+            if _ in choice_side: choice_side.remove(_)
+    elif data[x-6, y][0] != 21 and data[x-4, y][0] != 21:
+        for _ in (0, 3, 4, 5, 7, 8, 11, 12):
             if _ in choice_side: choice_side.remove(_)
 
-    elif data[x-6, y][0] != 21 and data[x-4, y][0] != 21:
-        for _ in (0, 3, 4, 5, 7, 8, 11, 12): 
+    #  up
+    if data[x, y+6][0] != 21 and data[x, y+4][0] == 21:
+        for _ in (1, 3, 5, 8, 9, 12, 13, 14):
+            if _ in choice_side: choice_side.remove(_)
+    elif data[x, y+6][0] != 21 and data[x, y+4][0] != 21:
+        for _ in (0, 2, 4, 6, 7, 10, 11, 15):
             if _ in choice_side: choice_side.remove(_)
 
     # down
     if data[x, y-6][0] != 21 and data[x, y-4][0] == 21:
-        for _ in (1, 3, 7, 10, 11, 12, 14, 15): 
+        for _ in (1, 3, 7, 10, 11, 12, 14, 15):
             if _ in choice_side: choice_side.remove(_)
-
-    elif data[x, y-6][0] != 21 and data[x, y-4][0] != 21: 
-        for _ in (0, 2, 4, 5, 6, 8, 9, 13): 
-            if _ in choice_side: choice_side.remove(_)
-
-    #  up
-    if data[x, y+6][0] != 21 and data[x, y+4][0] == 21: 
-        for _ in (1, 3, 5, 8, 9, 12, 13, 14): 
-            if _ in choice_side: choice_side.remove(_)
-
-    elif data[x, y+6][0] != 21 and data[x, y+4][0] != 21: 
-        for _ in (0, 2, 4, 6, 7, 10, 11, 15): 
+    elif data[x, y-6][0] != 21 and data[x, y-4][0] != 21:
+        for _ in (0, 2, 4, 5, 6, 8, 9, 13):
             if _ in choice_side: choice_side.remove(_)
 
     # more straight path
     # if 2 in choice_side: choice_side.append(2); choice_side.append(2); choice_side.append(2)
     # if 2 in choice_side: choice_side.append(2); choice_side.append(2); choice_side.append(2)
-    
-    if not 12<x<MAX_X-12 or not 12<y<MAX_Y-12 or iters > MAX_ITERS: 
-        for _ in range(4, 8): 
+
+    if not 12<x<(MAX_X-12) or not 12<y<(MAX_Y-12) or iters > MAX_ITERS:
+        for _ in range(4, 8):
             if _ in choice_side: choice_side = [_]
 
     # print(choice_side)
@@ -123,8 +118,9 @@ while len(working_stack):
     draw_side(x, y, side)
 
 print(f'iters: {iters}')
+
 image = Image.fromarray(data)
-image = image.transpose(Image.ROTATE_90)
-image = image.resize((RESIZE_TO[0], RESIZE_TO[1]), resample=Image.BOX)
+image = image.transpose(Image.Transpose.ROTATE_90)
+image = image.resize(RESIZE_TO, resample=Image.Resampling.BOX)
 image.save('image.png')
 image.show()
